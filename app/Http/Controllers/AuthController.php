87d6dccb036265e5176e907e597f4519a7da621c
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2023-06-01 18:12:47 +0200

    Tower to Lab

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        
+    }
+
+
+    public function login(LoginRequest $request)
+    {
+
+    }
+
+
+    public function logout(Request $request)
+    {
+
+    }
+}
